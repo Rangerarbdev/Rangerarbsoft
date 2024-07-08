c35b12680a6111f5fa3c5d89b3d1b7f7c9be73b1
Rangerarbdev <Rangearbdev@users.noreply.github.com>
2024-07-07 23:48:24 -0300

    Mostrar placeholder-buscar js

--- styles.js
+++ styles.js
@@ -1,4 +1,10 @@
-setcookie("cookie_name", "cookie_value", time() + 3600, "/", "", true, true);
+//-- setcookie("cookie_name", "cookie_value", time() + 3600, "/", "", true, true); --//
+//-- Mostrar/ocultar buscar(placeholder) --//
+const lupa = document.querySelector(".lupa");
+const muestraPlaceholder = document.querySelector(".icono-placeholder-image");
+lupa.addEventListener("click", () => {
+  muestraPlaceholder.classList.toggle("activado");
+});
 //-- Mostrar y ocultar ventana de enlaces-menu --//
 const logo = document.querySelector(".logo");
 const enlaces = document.querySelector(".container-enlaces-menu");
