df511198f04049275b3b8468c38df4e54f917a3e
Rangerarbdev <Rangearbdev@users.noreply.github.com>
2024-07-12 23:37:10 -0300

    Add code action container-rangerarbsoft js

--- styles.js
+++ styles.js
@@ -11,3 +11,20 @@ const enlaces = document.querySelector(".container-enlaces-menu");
 logo.addEventListener("click", () => {
   enlaces.classList.toggle("activado");
 });
+//-- Mostrar ventana cuenta personal --//
+//--const cuenta=document.querySelector('#cuenta-personal');const cuentaPersonal=document.querySelector('.vent-rigth');cuenta.addEventListener('click',() => {cuentaPersonal.classList.toggle('activado');});
+//-- Mostrar ventana de presentación de Nosotros --//
+const itemUno = document.querySelector(".item-uno");
+const contRangerarbsoft = document.querySelector(".container-rangerarbsoft");
+itemUno.addEventListener("click", () => {
+  contRangerarbsoft.classList.toggle("activado");
+});
+function mostrarRangerarbsoft() {
+  if (
+    (document.querySelector(".container-rangerarbsoft").style.display = "block")
+  );
+  document.querySelector(".contenedor-productos").style.display = "none";
+  document.querySelector(".contenedor-soporte").style.display = "none";
+  document.querySelector(".container-garantia").style.display = "none";
+  document.querySelector(".container-cotizacion").style.display = "none";
+}
